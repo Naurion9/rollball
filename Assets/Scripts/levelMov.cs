e81ce83ce6af7aaa0ff0e8db9d762327eecc626d
Naurion9 <ibu88@hotmail.com>
2018-01-18 20:52:47 +0100

    Organized gameobjects, resized maze and gyroscope script first try.

---
+++ Assets/Scripts/levelMov.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelMov : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Gyroscope gyro = Input.gyro;
+		if (!gyro.enabled){
+			gyro.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		transform.rotation = Input.gyro.attitude;		
+	}
+}
