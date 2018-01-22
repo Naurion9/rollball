40fb854b90b561eb50fdfe08f3913fd1940704b1
Naurion9 <ibu88@hotmail.com>
2018-01-22 01:59:26 +0100

    Added collision script to all holes.

---
+++ Assets/Scripts/falloff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class falloff : MonoBehaviour {
+
+	private Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider hole){
+
+		switch(hole.tag) {
+			case "hole": 
+				player.gameObject.SetActive (false);
+				Debug.Log ("Se cayó la bola. Game Over");
+				break;
+			case "meta":
+				Debug.Log ("Has completado el nivel!");
+				break;
+		}
+						
+	}
+}
+;
