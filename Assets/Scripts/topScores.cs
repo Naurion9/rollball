dda2521d6c7c06656822dc1393f319ba6c45ec87
Naurion9 <ibu88@hotmail.com>
2018-01-23 20:38:08 +0100

    Added SQLite library and testing scripts

---
+++ Assets/Scripts/topScores.cs
@@ -0,0 +1,10 @@
+﻿using SQLite4Unity3d;
+
+public class topScores{
+
+	[PrimaryKey, AutoIncrement]
+	public int id { get; set;}
+	public string name { get; set;}
+	public int score { get; set;}
+
+}
